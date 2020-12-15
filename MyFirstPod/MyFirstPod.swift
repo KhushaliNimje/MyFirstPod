15270bbebaf431c3e1dc91d1f0a62a24fb536e08
Khushali Nimje <khushali@caperminttechnologies.com>
2020-12-15 17:22:14 +0530

    commit

---
+++ MyFirstPod/MyFirstPod.swift
@@ -0,0 +1,21 @@
+//
+//  MyFirstPod.swift
+//  MyFirstPod
+//
+//  Created by Khushali Nimje on 15/12/20.
+//  Copyright © 2020 Khushali Nimje. All rights reserved.
+//
+
+import Foundation
+
+public final class MyFirstPod {
+    let name = "MyFirstPod"
+        
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- MyFirstPodTests/MyFirstPodTests.swift
+++ MyFirstPodTests/MyFirstPodTests.swift
@@ -10,6 +10,16 @@ import XCTest
 @testable import MyFirstPod
 
 class MyFirstPodTests: XCTestCase {
+    
+    var myFirstPod : MyFirstPod!
+    
+    override func setUp() {
+        myFirstPod = MyFirstPod()
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(myFirstPod.add(a: 1, b: 1), 2)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
