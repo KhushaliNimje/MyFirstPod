7fb774ea90dc1acec194741b8119193885a3950c
KhushaliNimje <73484326+KhushaliNimje@users.noreply.github.com>
2020-12-15 16:48:12 +0530

    Add files via upload

---
+++ MyFirstPod/MyFirstPod.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstPod.h
+//  MyFirstPod
+//
+//  Created by Khushali Nimje on 15/12/20.
+//  Copyright © 2020 Khushali Nimje. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstPod.
+FOUNDATION_EXPORT double MyFirstPodVersionNumber;
+
+//! Project version string for MyFirstPod.
+FOUNDATION_EXPORT const unsigned char MyFirstPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPod/PublicHeader.h>
+
+
---
+++ MyFirstPodExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstPodExample
+//
+//  Created by Khushali Nimje on 15/12/20.
+//  Copyright © 2020 Khushali Nimje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
